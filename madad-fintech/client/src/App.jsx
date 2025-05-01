fcb1b7966848617346145a4ddc8fdeb54ed75468
Mohd Shuaib <120455746+shuaib1997@users.noreply.github.com>
2025-05-01 13:29:23 +0300

    Update App.jsx

--- madad-fintech/client/src/App.jsx
+++ madad-fintech/client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LenderCalculator from './pages/LenderCalculator.jsx';
 import MsmePortal from './pages/MsmePortal.jsx'
 import LenderPortal from './pages/LenderPortel.jsx';
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<LenderCalculator />} />
         <Route path="/msme" element={<MsmePortal />} />
         <Route path="/lender" element={<LenderPortal />} />
