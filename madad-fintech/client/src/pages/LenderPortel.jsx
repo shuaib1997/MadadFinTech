39c20b90ebbaa50c8d9a00023afba77316f80e1c
Mohd Shuaib <120455746+shuaib1997@users.noreply.github.com>
2025-05-01 12:53:42 +0300

    Update LenderPortel.jsx

--- madad-fintech/client/src/pages/LenderPortel.jsx
+++ madad-fintech/client/src/pages/LenderPortel.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import LenderCalculator from '../components/LenderCalculator';
 
 export default function LenderPortal() {
   const [programForm, setProgramForm] = useState({
