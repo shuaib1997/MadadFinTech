899aa4a5bbc90f0a009c863d375feec6db75ae10
Mohd Shuaib <120455746+shuaib1997@users.noreply.github.com>
2025-05-01 14:59:22 +0300

    Update App.jsx

--- madad-fintech/client/src/App.jsx
+++ madad-fintech/client/src/App.jsx
@@ -1,17 +1,26 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './Component/Navbar.jsx';
 import LenderCalculator from './pages/LenderCalculator.jsx';
-import MsmePortal from './pages/MsmePortal.jsx'
+import MsmePortal from './pages/MsmePortal.jsx';
 import LenderPortal from './pages/LenderPortel.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="/dashboard" element={<LenderCalculator />} />
-        <Route path="/msme" element={<MsmePortal />} />
-        <Route path="/lender" element={<LenderPortal />} />
-      </Routes>
+      <div className="min-h-full">
+        <Navbar />
+        
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <Routes>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<LenderCalculator />} />
+              <Route path="/msme" element={<MsmePortal />} />
+              <Route path="/lender" element={<LenderPortal />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
